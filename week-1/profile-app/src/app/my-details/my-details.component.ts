98793d2e5c24290f5a4e0f2299e5f967f904de53
Dan Ross <dawross@my365.bellevue.edu>
2020-12-19 16:39:53 -0600

    1.5

--- week-1/profile-app/src/app/my-details/my-details.component.ts
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -1,10 +1,21 @@
+/*
+============================================
+; Title: Assignment 1.5 - Components
+; Author: Dan Ross
+; Date: 19 Dec 2020
+; Modified By: Dan Ross
+; Description: Details Components
+;===========================================
+*/
+//import
 import { Component, OnInit } from '@angular/core';
-
+//component
 @Component({
   selector: 'app-my-details',
   templateUrl: './my-details.component.html',
   styleUrls: ['./my-details.component.css']
 })
+//export
 export class MyDetailsComponent implements OnInit {
 
   constructor() { }
--- week-1/profile-app/src/app/my-image/my-image.component.ts
+++ week-1/profile-app/src/app/my-image/my-image.component.ts
@@ -1,10 +1,21 @@
+/*
+============================================
+; Title: Assignment 1.5 - Components
+; Author: Dan Ross
+; Date: 19 Dec 2020
+; Modified By: Dan Ross
+; Description: Image Components
+;===========================================
+*/
+//import
 import { Component, OnInit } from '@angular/core';
-
+//component
 @Component({
   selector: 'app-my-image',
   templateUrl: './my-image.component.html',
   styleUrls: ['./my-image.component.css']
 })
+//export
 export class MyImageComponent implements OnInit {
 
   constructor() { }
